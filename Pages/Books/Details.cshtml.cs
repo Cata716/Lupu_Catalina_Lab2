db525588223afca917aac8e151e30e111991bb66
Asus <Asus@DESKTOP-GCS9LO9>
2025-10-19 03:34:06 +0300

    Add project files.

---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lupu_Catalina_Lab2.Data;
+using Lupu_Catalina_Lab2.Models;
+
+namespace Lupu_Catalina_Lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Lupu_Catalina_Lab2.Data.Lupu_Catalina_Lab2Context _context;
+
+        public DetailsModel(Lupu_Catalina_Lab2.Data.Lupu_Catalina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                     .Include(b => b.Publisher)
+                     .Include(b => b.Author)
+                     .FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
